f0ed335dda4432e4202462c60d8bf772ba500396
Nguyen Ngo <mnguyenngo@gmail.com>
2018-06-03 16:34:10 -0700

    Scoring function enabled from pickled nlp data

--- src/get_nlp_data.py
+++ src/get_nlp_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scorer import Scorer
 import datetime as dt
+import re
 
 
 def get_nlp_data(path):
@@ -8,8 +9,8 @@ def get_nlp_data(path):
     data = pd.read_pickle(path)
 
     scorer = Scorer()
-
-    data['nlp_doc'] = data['definition'].apply(
+    data['clean_def'] = data['definition'].apply(clean_def)
+    data['nlp_doc'] = data['clean_def'].apply(
                         lambda x: add_nlp_doc(x, scorer))
 
     return data
@@ -24,6 +25,12 @@ def pickle_nlp_data(data, path):
 
     # save dataframe to pickle
     data.to_pickle(path + filename)
+
+
+def clean_def(definition):
+    if type(definition) == str:
+        definition = re.sub('\[(.*?)\]', ' ', definition)
+        return definition
 
 
 def add_nlp_doc(definition, nlp_model):
